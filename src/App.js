401c5ef8c3f468da015d33a1ea546f2a99653df0
Gaurav <gorav.dhamija@gmail.com>
2024-10-12 02:23:58 +0530

    Ecess data removed 9

--- src/App.js
+++ src/App.js
@@ -39,23 +39,7 @@ function App() {
     yearsLeft:0,
     deductible:35,
     deductibleCost:0 }
-  // ,
-  // {
-  //   loanid:191,
-  //   loanAmount: 535000,
-  //   termYears: 30,
-  //   interestRate: 2.750,
-  //   numberOfpaymentsMade: 36,
-  //   currentBalance: 0,
-  //   currentPayment: 0,
-  //   totalInterest:0,
-  //   interestPaid:0,
-  //   remainingInterest:0,
-  //   remainingPrincipal:0,
-  //   yearsLeft:0,
-  //   deductible:35,
-  //   deductibleCost:0 
-  // }
+  
 ]
 
 let [loans,setLoans] = useState(loansArray)
@@ -94,17 +78,7 @@ let [loans,setLoans] = useState(loansArray)
     payoffYears: 0,
     
   }
-  //,
-  // {
-  //   liabilityid:129,
-  //   liabilityName:Card,
-  //   balanceAmount: 0,
-  //   monthlyPayment: 200,
-  //   interestRate: 19.990,
-  //   interest: 0,
-  //   payoffYears: 0,
-    
-  // }
+  
 ]
 
 let [liabilities,setLiabilities] = useState(liabilitiesArray)
@@ -186,74 +160,12 @@ let proposalArray = [{
   benefitAfter5Year:0,
   totalPreviousLoanBalance:0
 }
-// {
-//   proposalid:500,
-//   proposalTerm:15,
-//   proposalInterestRate:2,
-//   proposalOriginationFeesRate:1,
-//   proposalOriginationFees: 0,
-//   proposalDiscountFeesRate:0,
-//   proposalDiscountFees:0,
-//   proposalMiscFeesRate:0,
-//   proposalMiscFees:3500,
-//   proposalLenderCreditRate:0,
-//   proposalLenderCredit:0,
-//   total:0,
-//   totalPayOff:0,
-//   inCC:0,
-//   newProposalLoanAmount:0,
-//   newPayment:0,
-//   apr:0,
-//   netSavingsPM:0,
-//   termReduction:0,
-//   totalNewInterest:0,
-//   interestSavedLost:0,
-//   taxBenefitBracket:35,
-//   taxBenefitNew:0,
-//   taxBenefitPrevious:0,
-//   investCashoutCheck:0,
-//   investCashoutAmount:0,
-//   rateOnInvest:5,
-//   investMonthlyCheck:0,
-//   investMonthlyAmount:0,
-//   benefitAfter1Year:0,
-//   benefitAfter3Year:0,
-//   benefitAfter5Year:0,
-//   totalPreviousLoanBalance:0
-  
-// }
+
 ]
 
 let [proposals,setProposals] = useState(proposalArray)
 
-  // let loanObj = {
-  //   loanAmount: 335000,
-  //   termYears: 30,
-  //   interestRate: 2.750,
-  //   numberOfpaymentsMade: 36,
-  //   currentBalance: 0,
-  //   currentPayment: 0,
-  //   totalInterest:0,
-  //   interestPaid:0,
-  //   remainingInterest:0,
-  //   remainingPrincipal:0,
-  //   yearsLeft:0,
-  //   deductible:35,
-  //   deductibleCost:0
-  // }
-
-  // let loansArray = [];
-
   
-  //   loansArray.push({...loanObj,loanid: Math.random()})
-
-  //   loansArray.push({...loanObj,loanid: Math.random()})
-
-  
-  // let [loans,setLoans] = useState(loansArray)
-  
- 
-
 
   return (
     <div>
